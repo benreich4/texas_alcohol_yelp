9a60702fe15f2a9e08d55e63fb2f8d00a149fb92
Ben Reich <Ben Reich>
2018-04-27 23:49:11 -0400

    Fix parens

--- map.js
+++ map.js
@@ -14,8 +14,8 @@ window.onload = function() {
 		var lat = parts[0];
 		var lon = parts[1];
 		if (lon > -106.76 && lon < -93.6 && lat < 36.53 && lat > 25.53) {
-			if (!(lat > 34.55 && lat < 40 && lon > -100 && lon < -94.75) {
-				if (!(lat > 32 && lat < 40 && lon > -108 && lon < -103) {
+			if (!(lat > 34.55 && lat < 40 && lon > -100 && lon < -94.75)) {
+				if (!(lat > 32 && lat < 40 && lon > -108 && lon < -103)) {
 					L.circleMarker([Number(parts[0]), Number(parts[1])], {
 						radius: 1,
 					  	renderer: myRenderer,
